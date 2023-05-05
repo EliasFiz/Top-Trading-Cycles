1f67fe148829c01c56dd432df525f59ccb0d69de
Elias Fizesan <eliasfizesan@yahoo.com>
2023-05-04 23:24:21 -0400

    initial object allocation now set to zero for al

--- generatePreferences.m
+++ generatePreferences.m
@@ -1,7 +1,6 @@
 function prefs = generatePreferences(num_agents, num_objects)
-    prefs = zeros(num_agents, num_objects);
-
-    for i = 1:num_agents
-        prefs(i, :) = randperm(num_objects);
-    end
+   prefs = zeros(num_agents, num_objects);
+   for i = 1:num_agents
+       prefs(i, :) = randperm(num_objects);
+   end
 end
--- iterate.m
+++ iterate.m
@@ -1,34 +1,29 @@
 function [updated_preferences updated_allocations] = iterate(prefs, allocations)
-    most_prefs = [];
-
-    % Replace 'length' with 'size(prefs, 1)' to get the number of agents
-    for index = 1:size(prefs, 1)
-        x = prefs(index, find(prefs(index, :) ~= -1, 1));
-
-        if ~isempty(x)
-            most_prefs = [most_prefs x];
-        else
-            most_prefs = [most_prefs -1];
-        end
-
-    end
-
-    cycles = detectCycles(most_prefs);
-    
-    for i = 1:length(cycles)
-        
-        cycle_nodes = cycles{i}{1};
-        cycle_edges = cycles{i}{2};
-
-        for j = 1:length(cycle_nodes)
-            prefs(cycle_nodes(j), :) = -1 * ones(1, size(prefs, 2));
-            prefs(prefs == cycle_nodes(j)) = -1;
-        end
-
-        for k = 1:length(cycle_edges)
-            allocations(cycle_edges{k}(1)) = cycle_edges{k}(2);
-        end
-    end
-    updated_preferences = prefs;
-    updated_allocations = allocations;
+   most_prefs = [];
+   for index = 1:length(prefs)
+       x = prefs(index, find(prefs(index, :) ~= -1, 1));
+       if ~isempty(x)
+           most_prefs = [most_prefs x];
+       else
+           most_prefs = [most_prefs -1];
+       end
+   end
+  
+   cycles = detectCycles(most_prefs);
+  
+   for i = 1:length(cycles)
+      
+       cycle_nodes = cycles{i}{1};
+       cycle_edges = cycles{i}{2};
+       for j = 1:length(cycle_nodes)
+           prefs(cycle_nodes(j), :) = -1 * ones(1, size(prefs, 2));
+           prefs(prefs == cycle_nodes(j)) = -1;
+       end
+       for k = 1:length(cycle_edges)
+           % Updated the line below to use the cycle_edges{k}(1) as the index and cycle_edges{k}(2) as the assigned value
+           allocations(cycle_edges{k}(1)) = cycle_edges{k}(2);
+       end
+   end
+   updated_preferences = prefs;
+   updated_allocations = allocations;
 end
